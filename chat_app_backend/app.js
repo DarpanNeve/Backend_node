d2128f6102aec3ce31cbf1987c150594ef898212
Darpan Neve <darpanneve3@gmail.com>
2023-10-19 20:14:59 +0530

    new changes

--- chat_app_backend/app.js
+++ chat_app_backend/app.js
@@ -1,31 +1,27 @@
-// Import the Express and HTTP modules.
 const express = require('express');
-const http = require("http");
+const http = require('http');
+// const path = require('path');
+const cors = require('cors'); // Import the CORS middleware.
 
-// Import the Socket.io module.
-const { Socket } = require('socket.io');
-
-// Create an Express app.
 const app = express();
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
 
-// Import the Path module.
-const path = require('path');
-
-// Set the port number.
-const port = process.env.port || 80;
+// Use the CORS middleware before your route handling.
+app.use(cors({ origin: '*' }));
 
 // Serve static files from the `public` directory.
-app.use(express.static(path.resolve("./public")));
+// app.use(express.static(path.resolve('./public')));
 
 // Create a Node.js HTTP server.
-const server = app.listen(port, () => {
-    console.log("server is started on", port);
+server.listen(port, () => {
+    console.log('Server is started on', port);
 });
 
 // Define a route to serve the index.html file.
-app.get('/', (req, res) => {
-    return res.sendFile("./public/index.html");
-});
+// app.get('/', (req, res) => {
+//     return res.sendFile('./public/index.html');
+// });
 
 // Create a Socket.io server.
 const io = require('socket.io')(server);
@@ -33,8 +29,8 @@ const io = require('socket.io')(server);
 // Listen for the `connection` event.
 io.on('connection', (socket) => {
     socket.on('message', (message) => {
-        io.emit('message',message);
-        console.log("message is", message);
-    })
-    console.log("connected successfully", socket.id);
+        io.emit('message', message);
+        console.log('message is', message);
+    });
+    console.log('connected successfully', socket.id);
 });
